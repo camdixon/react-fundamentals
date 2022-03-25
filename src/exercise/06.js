bcf660bdde95f04f808dcda3dd3142424af27f6b
Cameron Dixon <cameron.dixon@snowcommerce.com>
2022-03-25 00:18:31 -0400

    input refs are cool, and a hook

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,9 +4,10 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const usernameInputRef = React.useRef()
   function handleSubmit(event) {
     event.preventDefault()
-    const value = event.target.elements.usernameInput.value
+    const value = usernameInputRef.current.value
     onSubmitUsername(value)
   }
 
@@ -14,7 +15,7 @@ function UsernameForm({onSubmitUsername}) {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" />
+        <input ref={usernameInputRef} id="usernameInput" type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
